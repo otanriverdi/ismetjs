a72f1ebe1e3d6caced804bff76d0593e40bd28fb
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2020-11-09 23:05:27 +0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  testEnvironment: 'jest-environment-node',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src')],
+  collectCoverageFrom: ['**/src/**/*.ts'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+};
---
+++ src/bin/ismet.ts
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+// The above directive is mandatory for CLI entry points
+
+console.log('Hello, works!');
