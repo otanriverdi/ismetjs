fdd35179638e49070934fab5e950ca48d2bf343c
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2020-11-10 22:57:42 +0300

    feat(cli): entry meow instance

--- src/bin/ismet.ts
+++ src/bin/ismet.ts
@@ -3,5 +3,25 @@
 
 // This is the entry point for the CLI tool.
 
-// eslint-disable-next-line
-console.log("Hello, I'm Ismet!");
+import meow from 'meow';
+
+const cli = meow(
+  `
+	Usage
+	  $ ismet <directory>
+
+  Examples
+	  $ ismet
+	  🐙 your issues will be created 🐙
+`,
+  {
+    description:
+      '🐙 Automatically generates and manages git repo issues from code comments. ',
+  },
+);
+
+// main
+(function () {
+  // eslint-disable-next-line
+  console.log('Hello, world!');
+})();
