57b35269dad491b7895c795bb4e48e68e6c29b36
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2020-11-12 15:02:07 +0300

    fix(config): tokens are now stored system wide instead of per project

--- src/config/store.ts
+++ src/config/store.ts
@@ -7,29 +7,26 @@ import Conf from 'conf';
 export default class Store {
   private conf = new Conf();
 
-  // TODO better way?
-  private confId = process.cwd();
-
   /**
    * Saves the access token to the store..
    *
    * @param {string} token
    */
   setAccessToken(token: string): void {
-    this.conf.set(`${this.confId}.access_token`, token);
+    this.conf.set(`access_token`, token);
   }
 
   /**
    * Gets the access token saved inside the store.
    */
   getAccessToken(): string {
-    return this.conf.get(`${this.confId}.access_token`) as string;
+    return this.conf.get(`access_token`) as string;
   }
 
   /**
    * Deletes the access token saved inside the store.
    */
   deleteAccessToken(): void {
-    this.conf.delete(`${this.confId}.access_token`);
+    this.conf.delete(`access_token`);
   }
 }
